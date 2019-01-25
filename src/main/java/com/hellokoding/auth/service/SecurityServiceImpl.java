3796e4fe24390d08b8b7357c83aa50c536e4c9b9
Thembelani Silika <thembelani.silika@santam.co.za>
2019-01-25 15:02:33 +0200

    removed comment on SecurityServiceImpl.java

--- src/main/java/com/hellokoding/auth/service/SecurityServiceImpl.java
+++ src/main/java/com/hellokoding/auth/service/SecurityServiceImpl.java
@@ -20,10 +20,6 @@ public class SecurityServiceImpl implements SecurityService{
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
 
-    /**
-     *
-     * @return username
-     */
     @Override
     public String findLoggedInUsername() {
         Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
