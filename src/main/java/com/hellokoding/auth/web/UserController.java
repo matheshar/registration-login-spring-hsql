0a5703e7422bf9366dd695ad122c1fb34aea6d4f
Thembelani Silika <thembelani.silika@santam.co.za>
2019-01-31 14:03:40 +0200

    removed useless comments on UserController.java

--- src/main/java/com/hellokoding/auth/web/UserController.java
+++ src/main/java/com/hellokoding/auth/web/UserController.java
@@ -24,12 +24,7 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
-
-    /**
-     * 
-     * @param model
-     * @return
-     */
+    
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String registration(Model model) {
         model.addAttribute("userForm", new User());
